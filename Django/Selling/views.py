0a2d323d0fe41cea7819803507cd2b1263c35ab6
piseth04 <piseth3025@gmail.com>
2025-04-21 16:14:50 +0700

    Add user login and logout

--- Django/Selling/views.py
+++ Django/Selling/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product, Category
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Home page view
-def index(request):
+def home(request):
     # Fetch all products and categories from the database
     products = Product.objects.all()
     categories = Category.objects.all()
@@ -11,3 +13,28 @@ def index(request):
 # About page view
 def about(request):
     return render(request, 'about.html')
+
+# User page view
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in successfully."))
+            # Redirect to home page after login
+            return redirect('home')
+        else:
+            messages.success(request, ("Invalid username or password."))
+            # Corrected redirect to the login page
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+# Logout page view
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out successfully."))
+    # Redirect to home page after logout
+    return redirect('home')
